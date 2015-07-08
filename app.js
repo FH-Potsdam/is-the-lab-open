9c4e46d6e71dc6db27e4b3f5a33bb514d81c4e19
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2015-07-08 05:33:26 +0200

    app is working added express, server and routing

---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var hbs = require('express-hbs');
+var routes = require('./routes');
+// var allergene = require('./routes/allergene');
+// var favicon = require('serve-favicon');
+// var cookieParser = require('cookie-parser');
+
+var port = 4300;
+
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || port);
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs.express4({
+  defaultLayout: __dirname + '/views/layouts/default.hbs',
+  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/views/layouts'
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+// app.use(express.json());
+app.use(bodyParser.json());
+// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+if('development' == app.get('env')){
+  app.use(errorHandler());
+}
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.get('/', routes.index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// module.exports = app;
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var sheet = require("../app/getter");
+// console.log(sheet.get(id));
+// if(res !== null){
+//   var val = parseInt(res, 10);
+//   console.log(res);
+// }
+
+exports.index = function(req, res){
+
+
+var id =  "1DhfnhUP5qoL7LU5P5P7ZKOMavFHdVKeNW9w3AmKfhCA";
+sheet.get(id, function(err, val){
+  if(!err){
+  console.log(val);
+  if(val === 1){
+  res.render('index',{title:'Yes'});
+
+    console.log("Yes");
+  }else{
+  res.render('index',{title:'No'});
+
+    console.log("No");
+  }
+  }else{
+  res.render('index',{title:'Error retrieving data. Please come back later'});
+
+    console.error("Error %s", err);
+  }
+});
+
+  // res.render('index',{title:'Hello index.js'});
+};

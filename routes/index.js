a51000a520ee235af4114955ba208473acc03361
fabiantheblind <icke@fabianmoronzirfas.me>
2015-09-07 16:24:26 +0200

    added some style

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,32 @@ var sheet = require("../app/getter");
 //   console.log(res);
 // }
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
 
 
-var id =  "1DhfnhUP5qoL7LU5P5P7ZKOMavFHdVKeNW9w3AmKfhCA";
-sheet.get(id, function(err, val){
-  if(!err){
-  console.log(val);
-  if(val === 1){
-  res.render('index',{title:'Yes'});
+  var id = "1DhfnhUP5qoL7LU5P5P7ZKOMavFHdVKeNW9w3AmKfhCA";
+  sheet.get(id, function(err, val) {
+    if (!err) {
+      console.log(val);
+      if (val === 1) {
+        res.render('index', {
+          title: 'Is the lab open?',
+          val: 'Yes'
+        });
+        console.log("Yes");
+      } else {
+        res.render('index', {
+          title: 'Is the lab open?',
+          val: 'No'
+        });
+        console.log("No");
+      }
+    } else {
+      res.render('index', {
+        title: 'Error retrieving data. Please come back later'
+      });
 
-    console.log("Yes");
-  }else{
-  res.render('index',{title:'No'});
-
-    console.log("No");
-  }
-  }else{
-  res.render('index',{title:'Error retrieving data. Please come back later'});
-
-    console.error("Error %s", err);
-  }
-});
-
-  // res.render('index',{title:'Hello index.js'});
+      console.error("Error %s", err);
+    }
+  });
 };

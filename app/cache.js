a40b3a1faa7d7e320f318ae0e7224ca5edf5ef39
fabiantheblind <icke@fabianmoronzirfas.me>
2015-09-28 17:01:50 +0200

    some housekeeping

--- app/cache.js
+++ app/cache.js
@@ -1,4 +1,4 @@
-var sheet = require('../app/getter');
+// var sheet = require('../app/getter');
 var badge = require('./badge');
 var fs = require('fs');
 // console.log(sheet.get(id));
@@ -28,31 +28,35 @@ exports.apiWrite =function(value, dt){
       });
 };
 
-exports.write = function() {
-  // https://docs.google.com/spreadsheets/d/1j4lhA_I2iv6wWvsgvU9SjZxQCaSIWNNdBZZPMhDf7_4/edit?usp=sharing
-  var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
-  var id = settings.spreadID; //'1j4lhA_I2iv6wWvsgvU9SjZxQCaSIWNNdBZZPMhDf7_4';
-  sheet.get(id, function(err, val) {
-    if (!err) {
-      var now = new Date();
-      var tmstmp = now.getFullYear().toString() +
-        '-' + (now.getMonth() + 1).toString() +
-        '-' + now.getDate().toString() +
-        ' ' + now.getHours().toString() +
-        ':' + now.getMinutes().toString() +
-        ':' + now.getSeconds().toString();
-      console.log(val);
-      var data = {
-        'val': val,
-        'time': tmstmp,
-      };
-      fs.writeFile('public/cache/data.json', JSON.stringify(data), function(err) {
-        if (err) {
-          return console.error(err);
-        }
-      });
-    } else {
-      return console.error('Error %s', err);
-    }
-  });
-};
+/**
+ * LEGACY code not used anymore
+ * @return {[type]} [description]
+ */
+// exports.write = function() {
+//   // https://docs.google.com/spreadsheets/d/1j4lhA_I2iv6wWvsgvU9SjZxQCaSIWNNdBZZPMhDf7_4/edit?usp=sharing
+//   var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+//   var id = settings.spreadID; //'1j4lhA_I2iv6wWvsgvU9SjZxQCaSIWNNdBZZPMhDf7_4';
+//   sheet.get(id, function(err, val) {
+//     if (!err) {
+//       var now = new Date();
+//       var tmstmp = now.getFullYear().toString() +
+//         '-' + (now.getMonth() + 1).toString() +
+//         '-' + now.getDate().toString() +
+//         ' ' + now.getHours().toString() +
+//         ':' + now.getMinutes().toString() +
+//         ':' + now.getSeconds().toString();
+//       console.log(val);
+//       var data = {
+//         'val': val,
+//         'time': tmstmp,
+//       };
+//       fs.writeFile('public/cache/data.json', JSON.stringify(data), function(err) {
+//         if (err) {
+//           return console.error(err);
+//         }
+//       });
+//     } else {
+//       return console.error('Error %s', err);
+//     }
+//   });
+// };

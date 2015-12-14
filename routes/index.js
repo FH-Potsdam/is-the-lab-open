d7efd1f376178b048f7b1b75a60ed4687308e611
fabiantheblind <icke@fabianmoronzirfas.me>
2015-12-14 10:03:03 +0100

    prod vs dev

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var cache = require('../app/cache');
 exports.index = function(req, res) {
   var data = cache.read();
   res.render('index', {
-    env: req.app.get('dev'),
     title: 'Is the lab open?',
     val: (data.val === 1 ? 'yes!' : 'no!')
   });
